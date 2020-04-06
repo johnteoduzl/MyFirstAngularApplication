0193b19c9af68787e02f754c150f12e2d76911ea
John Odindo <johnteoduzl@users.noreply.github.com>
2020-04-06 13:52:52 +0300

    First Component
    I have made my first component and added the HTML as well to it.

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+  `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  </div>
+  `
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
-  pageTitle='My First Application';
+
+export class AppComponent
+{
+  pageTitle : string = "John Odindo Products";
 }
