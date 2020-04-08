635b0bcfefe06ea4c9fe7f88b6d49a869ad165fa
John Odindo <johnteoduzl@users.noreply.github.com>
2020-04-08 13:33:49 +0300

    Added Filtering
    Added the filtering producst to the ngFor in the product list html

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule} from'@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { from } from 'rxjs';
+import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces
   ],
   imports: [
     BrowserModule,
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -7,13 +7,26 @@ import { IProduct } from './product';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent
+export class ProductListComponent implements OnInit
 {
     pageTitle: string = "Product Listings";
     imageWidth: number =50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+
+    _listFilter: string = 'cart';
+
+    get listFilter():string{
+      return this._listFilter;
+    }
+    set listFilter(value:string)
+    {
+      this._listFilter=value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter):this.products;
+    }
+
+    filteredProducts:IProduct[];
     products : IProduct[]=[
         {
           "productId": 1,
@@ -67,9 +80,25 @@ export class ProductListComponent
         }
       ];
 
+      constructor()
+      {
+        this._listFilter='cart';
+        this.filteredProducts=this.products;
+      }
       toggleImage():void{
 
         this.showImage=!this.showImage;
+      }
+
+      ngOnInit():void{
+        console.log('In OnInit')
+      }
+
+
+      performFilter(filterBy: string):IProduct[]
+      {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product : IProduct)=> product.productName.toLocaleLowerCase().indexOf(filterBy)!==-1);
       }
 
 } 
