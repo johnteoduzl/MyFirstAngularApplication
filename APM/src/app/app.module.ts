a110829a584bb9b66be3c56540d6bf324f8fa071
John Odindo <johnteoduzl@users.noreply.github.com>
2020-04-09 17:04:44 +0300

    Adding Route Guards
    To the routes in the Application Module,Also added navigation back using in the Prodcut Detail Guard.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import { WelcomeComponent } from './home/welcome.component';
     HttpClientModule,
     RouterModule.forRoot([
       {path: 'products', component: ProductListComponent},
-      {path: 'products/:id',component:ProductDetailComponent},
+      {path: 'products/:id',component:ProductDetailComponent,canActivate:[ProductDetailGuard]},
       {path:'welcome', component: WelcomeComponent},
       {path:'',redirectTo: 'welcome',pathMatch: 'full'},
       {path:'**',redirectTo:'welcome',pathMatch:'full'}
