263755f0aa6b2c522fb6957bfac0a5ec4de0009f
John Odindo <johnteoduzl@users.noreply.github.com>
2020-04-09 13:31:43 +0300

    Added Rutes
    Added the routing module to the app module of the application and added various links to it.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from'@angular/forms';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -9,6 +10,7 @@ import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,20 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ProductListComponent,
     ConvertToSpaces,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id',component:ProductDetailComponent},
+      {path:'welcome', component: WelcomeComponent},
+      {path:'',redirectTo: 'welcome',pathMatch: 'full'},
+      {path:'**',redirectTo:'welcome',pathMatch:'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
